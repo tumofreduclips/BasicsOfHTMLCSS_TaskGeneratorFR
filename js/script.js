e9430d6f2e8086470d7b563fd494b2148478abef
tumofreduclips <40761105+tumofreduclips@users.noreply.github.com>
2019-04-10 14:09:55 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,48 @@
+function RandomColor() {
+	var letters = '0123456789ABCDEF';
+	var color = '#';
+	for (var i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+}
+
+function RandomPicture(){
+	var images = ['cat','butterfly','car','moto','baby','bike','football'];
+	var item = images[Math.floor(Math.random()*images.length)];
+
+	return (item);
+}
+
+function setRandomColor() {
+	var acolor = RandomColor();
+	$("a").css("color",acolor);
+	$(".acolor").html(acolor);
+
+	var linkbgcolor = RandomColor();
+	$("a").css("background-color",linkbgcolor);
+	$(".linkbgcolor").html(linkbgcolor);
+
+	var pcolor = RandomColor();
+	$("p").css("color",pcolor);
+	$(".pcolor").html(pcolor);
+
+	var h1color = RandomColor();
+	$("h1").css("color",h1color);
+	$(".h1color").html(h1color);
+
+	var h2color = RandomColor();
+	$("h2").css("color",h2color);
+	$(".h2color").html(h2color);
+}
+
+$(document).ready(
+	function(){
+		setRandomColor();
+		html2canvas(document.body).then(canvas => {
+			$("img").attr("src","img/"+RandomPicture()+".png");
+			$("img").css("width","300px");
+			// $("#capture").empty();
+			// $("#capture").html(canvas);
+		});
+	});
